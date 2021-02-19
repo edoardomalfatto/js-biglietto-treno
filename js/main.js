c5422ccbd18477c0afac9161f8d16c02267447e5
Edoardo <edoardo.malfatto@hotmail.it>
2021-02-19 18:09:51 +0100

    bonus aggiunto

--- js/main.js
+++ js/main.js
@@ -19,6 +19,22 @@ console.log(km);
 età=prompt("Quanti anni hai?");
 console.log(età);
 
+//Validazione input
+var numbers = /^[0-9999999999]+$/;
+
+if(età.match(numbers)) {
+ alert("Età correttamente inserita");
+} else {
+  alert("ATTENZIONE:Inserisci un'età in valori numerici");
+}
+
+if(km.match(numbers)) {
+  alert("Km correttamente inseriti");
+ } else {
+   alert("ATTENZIONE:Inserisci i Km in dati numerici");
+   document.getElementById('prezzo').innerHTML=("Inserisci i dati correttamente" ); 
+ }
+
 
 //Calcolo prezzo per età
 if (età < 18) {
@@ -28,11 +44,12 @@ if (età < 18) {
   } else {
     prezzo = (km * PrezzoAlKm );
   } 
+ 
   
+
 //Calcolo prezzo finale-approssimato
 var PrezzoFinale=Math.round((prezzo + Number.EPSILON) * 100) / 100;
   
 //Inietto risultato nell'HTML
 document.getElementById('prezzo').innerHTML=("Devi sganciare " + (PrezzoFinale) + " €" ); 
- 
  
